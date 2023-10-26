f162fedfee6d790cbc457a1f58e63c57794664ef
apesteguia <mikeltxomikeltxo@gmail.com>
2023-10-26 13:55:34 +0200

    editor

---
+++ src/lib/components/user/Editor.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let note: any;
+
+  function formatDate(dateString: any) {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    return new Date(dateString).toLocaleString(undefined, options);
+  }
+</script>
+
+<div class="absolute top-0 right-0 w-[calc(100%-300px)] h-full">
+  <div class="mt-5 ml-5">
+    <p class="font-bold">{note.title}</p>
+    <p class="text-sm">Última edición: {formatDate(note.last_edited)}</p>
+    <p class="text-sm">Creado: {formatDate(note.created_at)}</p>
+  </div>
+  <div class="w-full flex justify-center">
+    {note.content}
+  </div>
+</div>
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   export let data;
   import Navbar from "$lib/components/user/Navbar.svelte";
+  import Editor from "$lib/components/user/Editor.svelte";
   $: ({ user, tableData } = data);
 </script>
 
 <div class="absolute w-full min-h-full left-0 top-0 flex">
   <Navbar name={user.email} id={user.id} {data} noteId={tableData[0].id} />
+  <Editor note={tableData[0]} />
 </div>
